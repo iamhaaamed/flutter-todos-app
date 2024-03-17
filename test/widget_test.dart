b1e6369595a5885e5f6d6f5b6786df90dd5dd2f5
Hamed <hamed.azadegan@gmail.com>
2024-03-17 15:40:41 +0330

    feat: implement profile page UI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:flutter_todos_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(
+      onboardingViewed: false,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
